1fe4ad10dc0935c614e6ed1326bea578dc714b81
rushin-gh <rushin.mailme@gmail.com>
2026-03-30 09:08:16 +0530

    Rate limit controller and api setup

---
+++ apis/Controllers/RateLimit.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RateLimit : ControllerBase
+    {
+        [HttpGet("~/api/test")]
+        public IActionResult Test()
+        {
+            return Ok("API working");
+        }
+    }
+}
